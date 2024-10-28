028e3c194a21dcbbdc2d1cba66d33e08eb14b3bb
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2024-10-28 16:20:20 -0300

    add active class to header link depending on path

---
+++ public/js/header.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const headerLinks = document.querySelectorAll("header .links a");
+  const currentActive = document.querySelector(".currentPage");
+
+  currentActive.classList.remove("currentPage");
+  switch (window.location.pathname) {
+    case "/":
+      headerLinks[0].classList.add("currentPage");
+      break;
+    case "/news":
+      headerLinks[1].classList.add("currentPage");
+      break;
+    case "/races":
+      headerLinks[2].classList.add("currentPage");
+      break;
+    case "/drivers":
+      headerLinks[3].classList.add("currentPage");
+      break;
+    case "/teams":
+      headerLinks[4].classList.add("currentPage");
+    default:
+      break;
+  }
+});
