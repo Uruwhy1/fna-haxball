40ce6da39de1e8bebb99182982107c41fa264fce
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2024-10-30 12:21:41 -0300

    translate log in page to spanish xd

---
+++ public/js/form.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const forgottenPasword = document.querySelector(".forgotten-password");
+
+  forgottenPasword.addEventListener("click", () => {
+    alert("hablale a campi por discord queseyo");
+  });
+});
